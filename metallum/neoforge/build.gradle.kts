9e132e21a2c62557b2ebf3c1cd682146262436dc
Toybox System <system@toybox.quest>
2025-06-02 11:19:13 +0100

    Add options resources as existing sources for metallum.

--- metallum/neoforge/build.gradle.kts
+++ metallum/neoforge/build.gradle.kts
@@ -1,7 +1,23 @@
-import quest.toybox.template.Constants
-
 plugins {
     id("template-neoforge")
+}
+
+neoForge {
+    runs {
+        val common = findProject(":options:common")!!
+
+        named("data") {
+            programArguments.addAll(
+                "--existing", common.file("src/main/resources").absolutePath
+            )
+        }
+
+        named("commonData") {
+            programArguments.addAll(
+                "--existing", common.file("src/main/resources").absolutePath
+            )
+        }
+    }
 }
 
 dependencies {
